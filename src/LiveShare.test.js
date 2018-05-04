aee96d509691741528268b6450fe5648a9e3ddc6
Jonathan Carter <joncart@outlook.com>
2018-05-04 14:37:01 -0700

    Updating tests

--- src/LiveShare.test.js
+++ src/LiveShare.test.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-it('allows real-time editing', () => {
+it('allows real-time editing (with auto-completion!)', () => {
 });
 
-it('supports collaborative debugging', () => {
+it('supports collaborative debugging (with shared stepping!)', () => {
 });
 
-it('enables sharing remote terminals', () => {
+it('allows sharing locally running web apps (using a secure channel!)', () => {
+});
+
+it('enables sharing remote terminals (with either read-only or read/write access!)', () => {
 });
