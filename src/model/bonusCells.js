65f1271346fb42e85ff447a880f3defcf9b0dc06
Jonathan Carter <joncart@outlook.com>
2018-11-03 15:17:48 -0700

    Adding some JSDoc comments

--- src/model/bonusCells.js
+++ src/model/bonusCells.js
@@ -1,10 +1,22 @@
 const bonusCells = [18, 87, 131, 274, 412, 518];
 const specialCells = [37, 337];
 
+/**
+ * Determine whether a specific matrix cell represents a bonus cell.
+ * 
+ * @param {int} cellNumber The index of the cell within the signatures matrix to check.
+ * @returns {boolean} A boolean indicating whether the specified cell is a bonus cell or not.
+ */
 export function isBonusCell(cellNumber) {
     return bonusCells.includes(cellNumber);
 }
 
+/**
+ * Determine whether a specific matrix cell represents a special cell.
+ * 
+ * @param {int} cellNumber The index of the cell within the signatures matrix to check.
+ * @returns {boolean} A boolean indicating whether the specified cell is a special cell or not.
+ */
 export function isSpecialCell(cellNumber) {
     return specialCells.includes(cellNumber);
 }
