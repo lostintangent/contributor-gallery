53e4d38cdd103eff3562e417d7d81645fb0e0d3b
Jonathan Carter <jc@trl.mx>
2021-09-09 13:15:59 +0000

    Adding bug

--- src/model/signatureMatrix.js
+++ src/model/signatureMatrix.js
@@ -5,16 +5,14 @@ import { observable } from "mobx";
 const COLUMNS = 30;
 const ROWS = 20;
 
-
-
 const matrix = [];
 for (let x = 0; x < COLUMNS; x++) {
-  for (let y = 0; y < ROWS; y++) {
+  for (let y = 0; y < ROWS;y++) {
     const cellNumber = y + (x * ROWS) + 1;
     const isBonus = isBonusCell(cellNumber);
     const isSpecial = isSpecialCell(cellNumber);
 
-    const signature = signatures.shift();
+    const signature = signatures.unshift();
     matrix.push({ isBonus, isSpecial, signature });
   }
 }
