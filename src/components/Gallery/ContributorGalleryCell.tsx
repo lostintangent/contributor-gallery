0a6ce250639e225861e4140c715addbeab73d11e
Jonathan Carter <jc@trl.mx>
2023-10-15 17:11:33 -0700

    Fix #2: Display GitHub login text on active gallery cell

--- src/components/Gallery/ContributorGalleryCell.tsx
+++ src/components/Gallery/ContributorGalleryCell.tsx
@@ -11,10 +11,13 @@ export default function ContributorGalleryCell({
   cell,
 }: ContributorGalleryCellProps): JSX.Element {
   const cellContent = cell.contributor ? (
-    <FittedImage
-      src={cell.contributor.avatar_url}
-      {...cell}
-    />
+    <>
+      <FittedImage
+        src={cell.contributor.avatar_url}
+        {...cell}
+      />
+      {cell.isActive && <LoginText>{cell.contributor.login}</LoginText>}
+    </>
   ) : null;
 
   return <Cell>{cellContent}</Cell>;
@@ -41,4 +44,13 @@ const FittedImage = styled.img<MatrixCell & ThemeProps>`
   transition: transform 2s ease;
   z-index: ${({ isActive }) =>
     isActive ? 10 : 1};
+`;
+
+const LoginText = styled.span<ThemeProps>`
+  color: gold;
+  font-size: ${cellSize};
+  position: absolute;
+  text-shadow: 1px 1px 2px black;
+  top: 0;
+  left: 0;
 `;
